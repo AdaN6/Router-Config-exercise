b309a1a60723a9152a9a8c82d8e81253203639d6
AdaN6 <a_disney_da@hotmail.com>
2022-11-28 18:50:46 +0100

    created condition for 404 for non-exist single user

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -24,7 +24,9 @@ const getSingleUser = async (req, res) => {
         const {id} = req.params
         // Sanatising the id using dollar sign
         const {rows} = await pool.query(`SELECT * FROM users WHERE id=$1`, [id] )
-
+        // showing 404 when the id doesnt exist
+        if (!rows.length) return res.status(404).json({error: "User not found!"})
+        // return id from pool query
         return res.json({rows})
     } catch (error) {
         return res.status(500).json({error: error.message})
